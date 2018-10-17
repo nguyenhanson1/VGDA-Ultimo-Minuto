8cb789a482b8aee75be2d0bcb98942f12a5457c3
Hanson Nguyen <nguyenhanson1@gmail.com>
2018-10-16 21:39:25 -0700

    Camera Follow, might need some extra work.

---
+++ VGDA Project/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform target;//what camerca will follow
+    private Vector3 playerPosition;
+    private float OffsetZ;
+    private void Start()
+    {
+        playerPosition = target.position;
+        OffsetZ = (transform.position - target.position).z;
+    }
+    // Update is called once per frame
+    void Update () {
+        transform.position = target.TransformVector(target.position.x,target.position.y,target.position.z + OffsetZ);
+	}
+}
