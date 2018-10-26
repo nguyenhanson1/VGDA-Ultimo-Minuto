870cc80fdbf4dfcd9c80f767f2a9c8bd507c558d
Chloe Culver <c.culver831@gmail.com>
2018-10-26 03:47:04 -0700

    no message

--- VGDA Project/Assets/Scripts/EndGameCollision.cs
+++ VGDA Project/Assets/Scripts/EndGameCollision.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EndGameCollision : MonoBehaviour {
-
+public class EndGameCollision : MonoBehaviour
+{
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            Debug.Log("Object Enter");
+
+            Destroy(gameObject);
         }
+
+
     }
 }
