c8184f3addd07e6b16d2b4b29cdc05fa6daee2a7
Hanson Nguyen <nguyenhanson1@gmail.com>
2018-10-26 20:35:14 -0700

    Revert "Added player health and "death""
    This reverts commit 3aa1c07878ceb0971961d6062ff80705228f7901.

--- VGDA Project/Assets/Scripts/EndGameCollision.cs
+++ VGDA Project/Assets/Scripts/EndGameCollision.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EndGameCollision : MonoBehaviour
-{
+public class EndGameCollision : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            Debug.Log("We hit something!");
+            Debug.Log("Object Enter");
         }
     }
 }
