a61207e7e60020583cf18118fcb2ac304747b7b4
Hanson Nguyen <nguyenhanson1@gmail.com>
2018-10-26 20:42:48 -0700

    made "redSide" as a boolean value to check the value in order to make certain events

--- VGDA Project/Assets/changePolarity.cs
+++ VGDA Project/Assets/changePolarity.cs
@@ -2,26 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class changePolarity : MonoBehaviour {
+public class changePolarity : MonoBehaviour
+{
     public Material matOfObject;
     public Color newCol;
     public KeyCode changeColor;
-	// Use this for initialization
-	void Start () {
+    private bool redSide = true;
+    // Use this for initialization
+    void Start()
+    {
         matOfObject.color = Color.red;
-	}
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(changeColor))
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(changeColor))
         {
             if (matOfObject.color == Color.red)
             {
                 matOfObject.color = newCol;
+                redSide = false;
             }
             else
             {
                 matOfObject.color = Color.red;
+                redSide = true;
             }
         }
-	}
+    }
 }
