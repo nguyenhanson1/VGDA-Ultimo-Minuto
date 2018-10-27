16c0776c5ce5bc24af453ff691d81afeae533299
Chloe Culver <c.culver831@gmail.com>
2018-10-27 01:23:40 -0700

    Player Health and colliders to the chasers

---
+++ VGDA Project/Assets/Scripts/EnemyScripts/EnemyCollider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollider : MonoBehaviour
+{ 
+    public int damage;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        var hit = collision.gameObject;
+        var health = hit.GetComponent<PlayerHealth>();
+        if (collision.gameObject.tag == "Player")
+            if (health != null)
+        {
+                health.TakeDamage(damage);
+        }
+
+        Destroy(gameObject);
+    }
+}
+
+
---
+++ VGDA Project/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public const int maxHealth = 100;
+    public int currentHealth = maxHealth;
+    public RectTransform healthBar;
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Debug.Log("Dead!");
+        }
+
+        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+    }
+
+
+}
