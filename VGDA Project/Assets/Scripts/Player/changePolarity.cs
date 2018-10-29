eefa634674dd0cc96d71aff4b17299f23bf315c9
Chloe Culver <c.culver831@gmail.com>
2018-10-29 12:11:03 -0700

    no message

---
+++ VGDA Project/Assets/Scripts/Player/changePolarity.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changePolarity : MonoBehaviour
+{
+    public Material matOfObject;
+    public Color newCol;
+    public KeyCode changeColor;
+    private bool redSide = true;
+    // Use this for initialization
+    void Start()
+    {
+        matOfObject.color = Color.red;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(changeColor))
+        {
+            if (matOfObject.color == Color.red)
+            {
+                matOfObject.color = newCol;
+                redSide = false;
+            }
+            else
+            {
+                matOfObject.color = Color.red;
+                redSide = true;
+            }
+        }
+    }
+}
