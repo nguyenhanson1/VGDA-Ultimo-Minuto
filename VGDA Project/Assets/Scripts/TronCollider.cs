638d9438e2df42aaee073c40f054d1cf48783c03
Hanson Nguyen <nguyenhanson1@gmail.com>
2018-10-31 19:16:39 -0700

    Tron enemies will stay after colliding with player

---
+++ VGDA Project/Assets/Scripts/TronCollider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TronCollider : MonoBehaviour {
+    public int damage;
+    public bool enemyColorRed;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        string checkString = FindObjectOfType<ChangePolarity>().Check();
+        var hit = collision.gameObject;
+        var health = hit.GetComponent<PlayerHealth>();
+        if ((checkString == "red" & enemyColorRed == false) || (checkString == "blue" & enemyColorRed == true))
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                Debug.Log("Hit");
+                if (health != null)
+                {
+                    health.TakeDamage(damage);
+                }
+            }
+        }
+    }
+}
+
+
